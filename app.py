cc9a3d52e5d9c0777cb269fe86c9aa4fcf09ee07
eleonoraa007 <hrubenja.eleonora@gmail.com>
2025-02-03 12:41:37 +0100

    Deleted unnecessary data

--- app.py
+++ app.py
@@ -1,23 +1,3 @@
-# from flask import Flask
-# from flask_cors import CORS
-# from config import Config
-# from app.models import db
-# import os
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# CORS(app)
-
-# db.init_app(app)
-
-# from app.routes import *
-
-# # if __name__ == "__main__":
-# #     app.run(debug=True)
-
-# if __name__ == "__main__":
-#     app.run(debug=True if os.getenv("FLASK_DEBUG") == "True" else False)
-
 from app import create_app
 import os
 
--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 
-
 db = SQLAlchemy()
 
 class Task(db.Model):
